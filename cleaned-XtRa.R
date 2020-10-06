8e7b8336c7fb6347d3aa28420389b4bfc9a2428e
John Mutua <ymutua@gmail.com>
2020-10-06 11:58:53 +0300

    biomass work in progress

--- cleaned-XtRa.R
+++ cleaned-XtRa.R
@@ -36,3 +36,6 @@ nitrogen_balance <- nitrogen_balance(para, land_required, soil_erosion)
 
 # Compute meat and milk productivity
 livestock_productivity <- meat_milk_productivity(para)
+
+# Economics
+economics <- economics_payback(para, energy_required)
