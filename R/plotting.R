47a1068cc908fb3e51b5f5c9681f5e506b07e3ec
SOloo15 <oloostephen08@gmail.com>
2023-09-08 09:34:59 +0300

    plotting 1

--- R/plotting.R
+++ R/plotting.R
@@ -46,43 +46,91 @@ clean_plotting <- function(outFile,oDir){
     tt <- colnames(datos[2])
 
 
-    if(tt == "average_annual_milk_kg_yr"){
-      title = "Average annual milk (kg_yr)"
-    }else if(tt == "soil_mining_perc"){
-      title = "Soil mining (%)"
-    }else if (tt == "soil_leaching_perc"){
+    if(tt == "total_milk_produced_kg_fpcm_per_year"){
+      title = "Total milk FPCM (kg/yr)"
+    }else if(tt == "total_meat_produced_kg_per_year"){
+      title = "Total meat (kg/yr)"
+    }else if(tt == "total_protein_produced_kg_per_year"){
+      title = "Total protein (kg/yr)"
+    }else if (tt == "total_tlu"){
       title = "Soil leaching (%)"
-    }else if (tt == "erosion_tyr"){
+    }else if (tt == "total_land_requirement_ha"){
       title = "Erosion (t soil/yr)"
-    }else if (tt == "erosion_thayr"){
+    }else if (tt == "total_land_requirement_ha_per_kg_fpcm"){
       title = "Erosion (t soil/ha/yr)"
-    }else if (tt == "erosion_kgsoil_kg_fpcm"){
+    }else if (tt == "total_land_requirement_ha_per_kg_meat"){
       title = "Erosion (kg soil/ kg FPCM)"
-    }else if (tt == "land_requirement_ha"){
+    }else if (tt == "total_land_requirement_ha_per_kg_protein"){
       title = "Land required (ha/yr)"
-    }else if (tt == "total_land_required_ha_mt_fpcm"){
+    }else if (tt == "total_land_requirement_ha_per_tlu"){
       title = "Land required (ha/MT FPCM)"
-    }else if (tt == "ghgtot_t_co2eq_yr"){
+    }else if (tt == "total_n_balance_kg_n_per_year"){
       title = "GHG (t CO2eq/ha)"
-    }else if (tt == "ghgtot_t_co2eq_ha_yr"){
+    }else if (tt == "percent_area_mining"){
       title = "GHG (t CO2eq/ha/yr)"
-    }else if (tt == "ghgmilk_kg_co2eq_kg"){
+    }else if (tt == "percent_area_leaching"){
       title = "GHG (CO2eq/kg milk)"
-    }else if (tt == "ghgmeat_kg_co2eq_kg"){
+    }else if (tt == "n_balance_kg_n_per_ha_per_year"){
       title = "GHG (CO2eq/kg meat)"
-    }else if (tt == "ghgprotein_kg_co2eq_kg"){
+    }else if (tt == "n_balance_kg_n_per_kg_fpcm"){
       title = "GHG (CO2eq/kg protein)"
-    }else if (tt == "percent_of_precipitation_used_for_feed_production"){
+    }else if (tt == "n_balance_kg_n_per_kg_meat"){
       title = "% Precipitation use for feed production"
-    }else if (tt == "water_m3_yr"){
+    }else if (tt == "n_balance_kg_n_per_kg_protein"){
       title = "Water (m3/yr)"
-    }else if (tt == "waterha_m3_ha"){
+    }else if (tt == "erosion_t_soil_year"){
       title = "Water (m3/ha)"
-    }else if (tt == "water_use_perkg_fpcm"){
+    }else if (tt == "erosion_t_soil_per_ha_per_year"){
       title = "Water (m3/kg FPCM)"
-    }else if (tt == "water_use_perkg_meat"){
+    }else if (tt == "erosion_t_soil_per_kg_fpcm"){
       title = "Water (m3/kg meat)"
-    }else if (tt == "water_use_perkg_protein"){
+    }else if (tt == "erosion_t_soil_per_kg_meat"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "erosion_t_soil_per_kg_protein"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "ghg_emission_t_co2_eq_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "ghg_emission_t_co2_eq_per_ha_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "ghg_emission_t_co2_eq_per_kg_meat"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "ghg_emission_t_co2_eq_per_kg_protein"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "percent_precipitation_used_for_feed_production"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_water_use_m3"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_water_use_m3_per_ha"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_water_use_m3_per_kg_fpcm"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_water_use_m3_per_kg_meat"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_water_use_m3_per_kg_protein"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "carbon_stock_change_t_co2eq_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "carbon_stock_change_t_co2eq_per_ha_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "carbon_stock_change_t_co2eq_per_fpcm"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "carbon_stock_change_t_co2eq_per_meat"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "carbon_stock_change_t_co2eq_per_protein"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_milk_produced_energy_kcal_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_meat_produced_energy_kcal_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_milk_produced_ame_days_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_meat_produced_ame_days_per_year"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_carbon_balance_per_fpcm"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_carbon_balance_per_meat"){
+      title = "Water (m3/kg meat)"
+    }else if (tt == "total_carbon_balance_per_protein"){
       title = "Water (m3/kg protein)"
     }else{
       NA
@@ -92,7 +140,8 @@ clean_plotting <- function(outFile,oDir){
       geom_bar(stat = "identity",
                position = "dodge",
                width = 0.2) +
-      ggtitle(label = title)+
+      geom_text(aes(label = round(datos[,2],2)), position = position_dodge(width = 0.2), vjust = -0.5, size = 2) +
+      labs(x = "", y = tt, title = title, fill = "Scenario") +
       theme_bw()
 
     ggsave(paste0(oDir, tt, ".png"), width = 150, height = 100, units = "mm")
