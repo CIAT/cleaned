de1e456f66734c4c48a075ab824375d0783eae0a
M-Emmanuel <mwemamanue@gmail.com>
2026-01-08 09:18:49 +0300

    minor parentheses check

--- R/ghg_emission.R
+++ R/ghg_emission.R
@@ -84,15 +84,15 @@ ghg_emission <- function(para, energy_required, ghg_ipcc_data, land_required, ni
   }
 
   ym1 <- left_join(livestock,de1, by = c("livetype_code"="livestock_category_code"))%>%
-    mutate(ym = ifelse(ipcc_ef_category_t2 == "Dairy cows"& annual_milk > 8500 | annual_milk == 0) & de >= 0.7,table_10.12$Ym[1],
-                       ifelse(ipcc_ef_category_t2 == "Dairy cows"& annual_milk %gle% c(5000,8500) | annual_milk == 0) & de %gel% c(0.63,0.7),table_10.12$Ym[2],
-                              ifelse(ipcc_ef_category_t2 == "Dairy cows"& annual_milk <= 5000 |  annual_milk == 0) & de < 0.63,table_10.12$Ym[3],
+    mutate(ym = ifelse(ipcc_ef_category_t2 == "Dairy cows"& (annual_milk > 8500 | annual_milk == 0) & de >= 0.7,table_10.12$Ym[1],
+                       ifelse(ipcc_ef_category_t2 == "Dairy cows"& (annual_milk %gle% c(5000,8500) | annual_milk == 0) & de %gel% c(0.63,0.7),table_10.12$Ym[2],
+                              ifelse(ipcc_ef_category_t2 == "Dairy cows"& (annual_milk <= 5000 |  annual_milk == 0) & de < 0.63,table_10.12$Ym[3],
                                      ifelse(ipcc_ef_category_t2 == "Non-dairy" & de <= 0.62,table_10.12$Ym[4],
                                             ifelse(ipcc_ef_category_t2 == "Non-dairy" & de < 0.72 & de > 0.62,table_10.12$Ym[5],
                                                    ifelse(ipcc_ef_category_t2 == "Non-dairy" & de >= 0.72 & de < 0.75,table_10.12$Ym[6],
                                                           ifelse(ipcc_ef_category_t2 == "Non-dairy" & de >= 0.75,table_10.12$Ym[7],
                                                                  ifelse(ipcc_ef_category_t2 == "Sheep",table_10.13$Ym[1],
-                                                                        ifelse(ipcc_ef_category_t2 == "Goats",table_10.13$Ym[2],0)))))))))
+                                                                        ifelse(ipcc_ef_category_t2 == "Goats",table_10.13$Ym[2],0))))))))))
 
   #Computing methane enteric emission factor
 
@@ -114,9 +114,9 @@ ghg_emission <- function(para, energy_required, ghg_ipcc_data, land_required, ni
   region <- para[["region"]]
 
   productivity1 <-  ym1%>%
-    mutate(productivity=ifelse(ipcc_meth_man_category == "Dairy cows"& annual_milk > 8500 |annual_milk==0) & de >= 0.7,"High  productivity systems",
-                        ifelse(ipcc_meth_man_category == "Dairy cows"& annual_milk %gle% c(5000,8500) |annual_milk==0) & de %gel% c(0.63,0.7),"High  productivity systems",
-                               ifelse(ipcc_meth_man_category == "Dairy cows"& annual_milk <= 5000 |annual_milk==0) & de < 0.63,"Low  productivity systems",
+    mutate(productivity=ifelse(ipcc_meth_man_category == "Dairy cows"& (annual_milk > 8500 |annual_milk==0) & de >= 0.7,"High  productivity systems",
+                        ifelse(ipcc_meth_man_category == "Dairy cows"& (annual_milk %gle% c(5000,8500) |annual_milk==0) & de %gel% c(0.63,0.7),"High  productivity systems",
+                               ifelse(ipcc_meth_man_category == "Dairy cows"& (annual_milk <= 5000 |annual_milk==0) & de < 0.63,"Low  productivity systems",
                                       ifelse(ipcc_meth_man_category == "Other cattle" & de <= 0.62,"Low  productivity systems",
                                              ifelse(ipcc_meth_man_category == "Other cattle" & de < 0.72 & de > 0.62,"Low  productivity systems",
                                                     ifelse(ipcc_meth_man_category == "Other cattle" & de >= 0.72 & de < 0.75,"High  productivity systems",
