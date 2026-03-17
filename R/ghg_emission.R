a23abd9215f6d068b4dc77fa0aa055bbb27c07ac
M-Emmanuel <mwemamanue@gmail.com>
2026-03-17 11:51:29 +0300

    update climate picker

--- R/ghg_emission.R
+++ R/ghg_emission.R
@@ -162,13 +162,13 @@ ghg_emission <- function(para, energy_required, ghg_ipcc_data, land_required, ni
   table_10.17 <- ghg_ipcc_data[["Table 10.17"]][ghg_ipcc_data[["Table 10.17"]]$climate==climate_zone,]
 
   mcf <- max_meth_bo%>%
-    left_join(table_10.17[,c(1,5)], by = c("manureman_non_roofed_enclosure"="Manure_management_systems"))%>%
+    left_join(table_10.17[,c(1,4)], by = c("manureman_non_roofed_enclosure"="Manure_management_systems"))%>%
     rename(mfc_non_roofed_enclosure=MCFs)%>%
-    left_join(table_10.17[,c(1,5)], by = c("manureman_offfarm_grazing"="Manure_management_systems"))%>%
+    left_join(table_10.17[,c(1,4)], by = c("manureman_offfarm_grazing"="Manure_management_systems"))%>%
     rename(mfc_offfarm_grazing=MCFs)%>%
-    left_join(table_10.17[,c(1,5)], by = c("manureman_onfarm_grazing"="Manure_management_systems"))%>%
+    left_join(table_10.17[,c(1,4)], by = c("manureman_onfarm_grazing"="Manure_management_systems"))%>%
     rename(mfc_onfarm_grazing=MCFs)%>%
-    left_join(table_10.17[,c(1,5)], by = c("manureman_stable"="Manure_management_systems"))%>%
+    left_join(table_10.17[,c(1,4)], by = c("manureman_stable"="Manure_management_systems"))%>%
     rename(mfc_stable=MCFs)%>%
     select(-de_intake,-ge_intake,-de,-er_growth,-dmi_tot)
 
